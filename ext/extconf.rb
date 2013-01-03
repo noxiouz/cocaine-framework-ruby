86e5efd01daff92dc764a2fab555b90c94dbab49
noxiouz <noxiouz@yandex.ru>
2013-01-03 21:00:12 +0400

    Some clean

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,8 +1,9 @@
 require 'mkmf'
+
+library_name = "Client"
+
 have_library("stdc++")
 have_library("cocaine-dealer")
 find_header("c_dealer.hpp","./include/")
-find_header("c_message.hpp")
-find_header("c_response.hpp")
 
-create_makefile("Client")
+create_makefile(library_name, 'src')
